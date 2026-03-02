30aac34dd9867ba48e7ba6c348b0062c207d5e3a
Justice Osei Sarfo <sarfojustice96@gmail.com>
2026-03-02 19:25:52 +0000

    configure test setup with in-memory mongodb

--- tests/setup.js
+++ tests/setup.js
@@ -1,3 +1,21 @@
-import app from "../src/app.js";
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import dotenv from "dotenv";
 
-export default app;
+dotenv.config();
+
+let mongoServer;
+
+// Set JWT_SECRET for tests if not set
+process.env.JWT_SECRET = process.env.JWT_SECRET || "testsecret123";
+
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  const mongoUri = mongoServer.getUri();
+  await mongoose.connect(mongoUri);
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoServer.stop();
+});
